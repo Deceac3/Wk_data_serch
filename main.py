96ce6c9dd1975752caa43df1e216a92af9db1c56
Роман Ветюгов <romab1cool1@mail.ru>
2024-07-09 20:53:07 +1100

    немного меняем структуру

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
-import requests
-
-st_accept = "text/html"
+from selenium import webdriver as wd
+import time
 
 server = "https://vk.com/deceasedep"
 
-st_useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+#st_useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
 
-headers = {
-   "Accept": st_accept,
-   "User-Agent": st_useragent
-}
+browser = wd.Chrome("/usr/bin/chromedriver/")
 
-req = requests.get(server, headers)
-
-src = req.text
-
-print(src.find("Р"))
-
-print(src[0:][:1000])
+browser.quit() 
